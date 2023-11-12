ad005692c52acabfc9b3f024f618d2a577652dcf
Petar Markov <petar@petarmarkov.co.uk>
2023-11-12 23:42:59 +0000

    fix(scroll)

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -11,37 +11,52 @@
 </svelte:head>
 
 <!-- Main Content -->
-<main class="text-white max-w-screen-md w-full min-h-full">
-    <!--Topbar with title-->
-    <div class="flex flex-row items-center justify-between w-full h-16 px-4 bg-gray-900 border-b border-gray-800 max-h-screen">
-        <div class="flex flex-row items-center justify-center w-8 h-8 mr-2 bg-gray-800 rounded-full">
-            <Icon icon="ic:baseline-last-page" class="w-6 h-6 text-gray-400 font-black"/>
-        </div>
-        <h1 class="text-lg font-semibold">{title}</h1>
-        <div class="flex-grow"></div>
-        <!--Buttons-->
-        {#each buttons as button}
-            <div class="flex flex-row items-center justify-center w-8 h-8 ml-2 bg-gray-800 rounded-full text-gray-400">
-                {#if button?.href}
-                    <a href={button.href}>
-                        <Icon icon={button.icon}/>
-                    </a>
-                {:else}
-                    <button on:click={button.action}>
-                        <Icon icon={button.icon}/>
-                    </button>
-                {/if}
+<main class="text-white max-w-screen-md w-full min-h-screen max-h-screen">
+    <div class="h-screen max-h-screen min-h-screen">
+        <div class="flex relative flex-row items-center justify-between w-full h-16 px-4 bg-gray-900 border-b border-gray-800">
+            <div class="flex flex-row items-center justify-center w-8 h-8 mr-2 bg-gray-800 rounded-full">
+                <Icon icon="ic:baseline-last-page" class="w-6 h-6 text-gray-400 font-black"/>
             </div>
-        {/each}
-    </div>
+            <h1 class="text-lg font-semibold">{title}</h1>
+            <div class="flex-grow"></div>
+            {#each buttons as button}
+                <div class="flex flex-row items-center justify-center w-8 h-8 ml-2 bg-gray-800 rounded-full text-gray-400">
+                    {#if button?.href}
+                        <a href={button.href}>
+                            <Icon icon={button.icon}/>
+                        </a>
+                    {:else}
+                        <button on:click={button.action}>
+                            <Icon icon={button.icon}/>
+                        </button>
+                    {/if}
+                </div>
+            {/each}
+        </div>
 
-    <!--Buttons-->
-
-    <div class="relative overflow-y-auto">
-        <slot/>
+        <div class="relative overflow-y-auto" style="height: calc(100vh - 4rem);"> <!-- h-16 = 4rem -->
+            <slot/>
+        </div>
     </div>
 </main>
 
 <div class="flex-col relative items-start w-full min-h-screen h-full hidden xl:flex max-w-xs mr-auto border-l border-gray-800">
 
 </div>
+
+<style>
+    /* width */
+    ::-webkit-scrollbar {
+        width: 1px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        background: #111827;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #1e3a8a;
+    }
+</style>
