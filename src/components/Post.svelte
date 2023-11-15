1761bd06de1ecdb90566421b6b9e4e543ee4fcbd
Petar Markov <petar@petarmarkov.co.uk>
2023-11-15 23:13:36 +0000

    fix(post): time not updating realtime

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,59 +1,63 @@
 <script>
-	import {onMount} from 'svelte';
-	import sanitizeHtml from 'sanitize-html';
-	import {marked} from 'marked';
+  import { browser } from "$app/environment";
+  import sanitizeHtml from "sanitize-html";
+  import { marked } from "marked";
 
-	let currentTime = new Date();
+  let currentTime = new Date();
+  if (browser) {
+    setInterval(() => {
+      currentTime = new Date();
+    }, 1000);
+  }
 
-	onMount(() => {
-		const interval = setInterval(() => {
-			currentTime = new Date();
-		}, 1000);
+  let formattedTime;
 
-		return () => {
-			clearInterval(interval);
-		};
-	});
+  $: currentTime, formattedTime = post ? timeSince(post?.created_at) : "";
 
-	function timeSince(postDate) {
-		const date = new Date(postDate);
-		let elapsedSeconds = Math.floor((currentTime.getTime() - date.getTime()) / 1000);
+  function timeSince(postDate) {
+    const date = new Date(postDate);
+    let elapsedSeconds = Math.floor((currentTime.getTime() - date.getTime()) / 1000);
 
-		const intervals = [
-			{seconds: 31536000, name: "y"},
-			{seconds: 2592000, name: "mo"},
-			{seconds: 86400, name: "d"},
-			{seconds: 3600, name: "h"},
-			{seconds: 60, name: "m"},
-			{seconds: 1, name: "s"},
-		];
+    const intervals = [
+      { seconds: 31536000, name: "y" },
+      { seconds: 2592000, name: "mo" },
+      { seconds: 86400, name: "d" },
+      { seconds: 3600, name: "h" },
+      { seconds: 60, name: "m" },
+      { seconds: 1, name: "s" }
+    ];
 
-		for (const interval of intervals) {
-			if (elapsedSeconds >= interval.seconds) {
-				return Math.floor(elapsedSeconds / interval.seconds) + interval.name;
-			}
-		}
-	}
+    for (const interval of intervals) {
+      if (elapsedSeconds >= interval.seconds) {
+        const count = Math.floor(elapsedSeconds / interval.seconds);
+        return `${count}${interval.name}`;
+      }
+    }
+    return "0s";
+  }
 
-	export let post;
+  export let post;
+  let sanitizedContent;
 
-	let sanitizedContent = sanitizeHtml(marked(post.content));
+  $: if (post) {
+    sanitizedContent = sanitizeHtml(marked(post.content));
+  }
 </script>
 
 <li class="overflow-hidden shadow flex flex-col w-full bg-gray-950 rounded">
-    <div class="flex flex-row items-center gap-4 h-16 p-3">
-        <!--<img class="w-10 h-10 rounded-full"
-             src="https://via.placeholder.com/150"
-             alt={""}>-->
-        <div class="flex flex-col">
-            <div class="text-white font-medium">{post?.user?.display_name}</div>
-            <div class="text-gray-400 font-medium text-xs">@{post?.user?.username}</div>
-        </div>
-        <div class="flex-grow"></div>
-        <div class="text-gray-400 text-xs h-full">{timeSince(post?.created_at)}</div>
+  <div class="flex flex-row items-center gap-4 h-16 p-3">
+    <!--<img class="w-10 h-10 rounded-full"
+         src="https://via.placeholder.com/150"
+         alt={""}>-->
+    <div class="flex flex-col">
+      <div class="text-white font-medium">{post?.user?.display_name}</div>
+      <div class="text-gray-400 font-medium text-xs">@{post?.user?.username}</div>
     </div>
-    <hr class="border-gray-800">
-    <div class="p-4 text-sm font-medium text-font">
-        {@html sanitizedContent}
-    </div>
+    <div class="flex-grow"></div>
+    <div class="text-gray-400 text-xs h-full">{formattedTime}</div>
+  </div>
+  <hr class="border-gray-800">
+  <div class="p-4 text-sm font-medium text-font">
+    {@html sanitizedContent}
+  </div>
 </li>
