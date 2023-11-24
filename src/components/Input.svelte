f453ea219e69ed595599ac25503f6059c6ec36d4
Petar Markov <petar@petarmarkov.co.uk>
2023-11-24 12:37:05 +0000

    ref(Input): twoway binding

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <label for={id} class="flex flex-col gap-2 w-full bg-gray-950 rounded-md">
-    <input id={id} type={type} bind:value={value} class="w-full bg-gray-800 rounded-md">
+    <input id={id} {...{type}} bind:value={value} class="w-full bg-gray-800 rounded-md">
 </label>
