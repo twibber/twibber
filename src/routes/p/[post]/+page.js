cee96c791a9f3f12bd8d1e2b49a442daf680592e
Petar Markov <petar@petarmarkov.co.uk>
2023-11-26 21:45:27 +0000

    setup routes for posts

---
+++ src/routes/p/[post]/+page.js
@@ -0,0 +1,11 @@
+import {request} from "$lib/request.js";
+
+export const load = (async ({fetch, params}) => {
+	return {
+		post: await request({
+			method: 'GET',
+			url: `/posts/${params.post}`,
+			fetchHandler: fetch,
+		}).catch(() => null).then((res) => res?.body?.data)
+	}
+})
---
+++ src/routes/p/[post]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Display from "$components/Display.svelte";
+
+	export let data;
+</script>
+
+<Display title={"Post"}>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+</Display>
